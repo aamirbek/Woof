c4e8c46012b42eae4362314f1ed78174f6baf742
A.A <167234045+aamirbek@users.noreply.github.com>
2026-06-22 19:56:39 +0200

    Added expandable dog cards

--- app/src/main/java/com/example/woof/MainActivity.kt
+++ app/src/main/java/com/example/woof/MainActivity.kt
@@ -5,24 +5,44 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ExpandLess
+import androidx.compose.material.icons.filled.ExpandMore
+
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+
 import com.example.woof.data.Dog
 import com.example.woof.data.dogs
 import com.example.woof.ui.theme.WoofTheme
@@ -50,9 +70,6 @@ class MainActivity : ComponentActivity() {
 }
 
 
-/**
- * Displays the list of dogs
- */
 @Composable
 fun WoofApp() {
 
@@ -62,157 +79,313 @@ fun WoofApp() {
 
             DogItem(
                 dog = it,
+
                 modifier = Modifier.padding(
                     dimensionResource(
                         R.dimen.padding_small
                     )
                 )
             )
-
         }
     }
 }
 
 
-/**
- * Displays one dog card
- */
 @Composable
 fun DogItem(
     dog: Dog,
     modifier: Modifier = Modifier
 ) {
 
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+
     Card(
         modifier = modifier
     ) {
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(
-                    dimensionResource(
-                        R.dimen.padding_small
+        Column {
+
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(
+                        dimensionResource(
+                            R.dimen.padding_small
+                        )
+                    )
+            ) {
+
+
+                DogIcon(
+                    dog.imageResourceId
+                )
+
+
+                DogInformation(
+                    dog.name,
+                    dog.age
+                )
+
+
+                Spacer(
+                    modifier = Modifier.weight(1f)
+                )
+
+
+                DogItemButton(
+
+                    expanded = expanded,
+
+                    onClick = {
+
+                        expanded = !expanded
+
+                    }
+                )
+            }
+
+
+            if (expanded) {
+
+                DogHobby(
+
+                    dog.hobbies,
+
+                    modifier = Modifier.padding(
+                        start = dimensionResource(
+                            R.dimen.padding_medium
+                        ),
+
+                        top = dimensionResource(
+                            R.dimen.padding_small
+                        ),
+
+                        bottom = dimensionResource(
+                            R.dimen.padding_medium
+                        )
                     )
                 )
-        ) {
-
-            DogIcon(
-                dog.imageResourceId
-            )
-
-            DogInformation(
-                dog.name,
-                dog.age
-            )
-
+            }
         }
     }
 }
 
 
-/**
- * Displays dog image
- */
 @Composable
-fun DogIcon(
-    @DrawableRes dogIcon: Int,
+private fun DogItemButton(
+    expanded: Boolean,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
+
+
+    IconButton(
+
+        onClick = onClick,
+
+        modifier = modifier
+
+    ) {
+
+
+        Icon(
+
+            imageVector = if (expanded) {
+
+                Icons.Filled.ExpandLess
+
+            } else {
+
+                Icons.Filled.ExpandMore
+            },
+
+
+            contentDescription = stringResource(
+                R.string.expand_button_content_description
+            ),
+
+
+            tint =
+                MaterialTheme.colorScheme.secondary
+        )
+    }
+}
+
+
+@Composable
+fun DogIcon(
+
+    @DrawableRes dogIcon: Int,
+
+    modifier: Modifier = Modifier
+
+) {
+
 
     Image(
 
         modifier = modifier
+
             .size(
                 dimensionResource(
                     R.dimen.image_size
                 )
             )
+
+
             .padding(
                 dimensionResource(
                     R.dimen.padding_small
-            mnu    )
+                )
             )
+
+
             .clip(
                 MaterialTheme.shapes.small
             ),
 
-        contentScale = ContentScale.Crop,
 
-        painter = painterResource(
-            dogIcon
-        ),
+        contentScale =
+            ContentScale.Crop,
+
+
+        painter =
+            painterResource(
+                dogIcon
+            ),
+
 
         contentDescription = null
     )
 }
 
 
-/**
- * Displays dog name and age
- */
 @Composable
 fun DogInformation(
+
     @StringRes dogName: Int,
+
     dogAge: Int,
+
     modifier: Modifier = Modifier
+
 ) {
+
 
     Column(
         modifier = modifier
     ) {
 
+
         Text(
-            text = stringResource(
-                dogName
-            ),
-            modifier = Modifier.padding(
-                top = dimensionResource(
-                    R.dimen.padding_small
+
+            text =
+                stringResource(
+                    dogName
+                ),
+
+
+            style =
+                MaterialTheme.typography.displayMedium,
+
+
+            modifier =
+                Modifier.padding(
+                    top = dimensionResource(
+                        R.dimen.padding_small
+                    )
                 )
-            )
         )
+
 
         Text(
-            text = stringResource(
-                R.string.years_old,
-                dogAge
-            )
-        )
 
+            text =
+                stringResource(
+                    R.string.years_old,
+                    dogAge
+                ),
+
+
+            style =
+                MaterialTheme.typography.bodyLarge
+        )
     }
 }
 
 
-/**
- * Light Theme Preview
- */
+@Composable
+fun DogHobby(
+
+    @StringRes dogHobby: Int,
+
+    modifier: Modifier = Modifier
+
+) {
+
+
+    Column(
+        modifier = modifier
+    ) {
+
+
+        Text(
+
+            text =
+                stringResource(
+                    R.string.about
+                ),
+
+
+            style =
+                MaterialTheme.typography.labelSmall
+        )
+
+
+        Text(
+
+            text =
+                stringResource(
+                    dogHobby
+                ),
+
+
+            style =
+                MaterialTheme.typography.bodyLarge
+        )
+    }
+}
+
+
 @Preview
 @Composable
 fun WoofPreview() {
+
 
     WoofTheme(
         darkTheme = false
     ) {
 
-        WoofApp()
 
+        WoofApp()
     }
 }
 
 
-/**
- * Dark Theme Preview
- */
 @Preview
 @Composable
 fun WoofDarkThemePreview() {
+
 
     WoofTheme(
         darkTheme = true
     ) {
 
-        WoofApp()
 
+        WoofApp()
     }
 }
