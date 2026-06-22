660c4fe57e8582b4201c67d4052b8fed47046aef
A.A <167234045+aamirbek@users.noreply.github.com>
2026-06-22 18:54:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/woof/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.example.woof.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val md_theme_light_primary = Color(0xFF006C4C)
+val md_theme_light_onPrimary = Color(0xFFFFFFFF)
+val md_theme_light_background = Color(0xFFFBFDF9)
+
+val md_theme_dark_primary = Color(0xFF6CDBAC)
+val md_theme_dark_background = Color(0xFF191C1A)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
